f7f49d6ba61dcc87129c3ccba7f4d45844945197
amirhosein <amirhoseinbu80@gmail.com>
2024-04-26 00:09:11 +0330

    build a login view

--- auths/views.py
+++ auths/views.py
@@ -23,3 +23,31 @@ def signup_view(request):
 
     context = {'form':form}
     return render(request,'auths/registration/signup.html',context)
+
+def login_view(request):
+
+    if request.user.is_authenticated:
+        messages.warning(request,"Hey You are already LoggedIn")
+        # return redirect("web:index")
+
+    if request.method=='POST':
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+
+        try:
+            CustomUser.objects.get(email = email)
+            user = authenticate(request,email=email , password = password)
+
+            if user is not None:
+                login (request, user)
+                messages.success(request,"You are LoggedIn")
+                # return redirect('web:index')
+            else:
+                messages.warning(request,'User Does not exist create an account')
+
+        except:
+            messages.warning(request,f'User with {email} Does Not Exist')
+
+
+
+    return render(request,'auths/registration/login.html')
