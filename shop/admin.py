cbd611325924b227a798eb0a9675ecc896e56e70
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-26 02:14:51 +0330

    add ProductImagesAdmin class to manage ProductImages inline th admin interface

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import (Product,Category,Vendor,CartOrder,CartOrderItem,Wishlist,ProductImages,ProductReview,Address )
 # Register your models here.
+
+
+class ProductImagesAdmin(admin.TabularInline):
+
+    model = ProductImages
