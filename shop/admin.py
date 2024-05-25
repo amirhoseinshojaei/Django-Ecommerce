bb76948e274b65955de4e18be34ed15cf53fda3c
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-26 02:23:39 +0330

    Add ProductAdmin class with inline ProductImagesAdmin and custom list display

--- shop/admin.py
+++ shop/admin.py
@@ -6,3 +6,15 @@ from .models import (Product,Category,Vendor,CartOrder,CartOrderItem,Wishlist,Pr
 class ProductImagesAdmin(admin.TabularInline):
 
     model = ProductImages
+
+
+
+class ProductAdmin(admin.ModelAdmin):
+
+    inlines = [ProductImagesAdmin]
+    list_display =[
+
+        'user','title','product_image','price','featured','product_status'
+    ]
+
+
