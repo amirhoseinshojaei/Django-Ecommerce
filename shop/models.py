701a447df903ccc2b36d2ef02d7c2fb9e78593d6
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-25 23:00:56 +0330

    build Product and Product Image model & updated requirements

--- shop/models.py
+++ shop/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from shortuuidfield import ShortUUIDField
 from django.utils.html import mark_safe
 from django.conf import settings
+from taggit.managers import TaggableManager
 # Create your models here.
 
 
@@ -49,5 +50,66 @@ class Vendor(models.Model):
     def __str__(self):
         return self.title
     
+
+
+class Product(models.Model):
+
+    pid = ShortUUIDField(unique = True, max_length=20)
+    category = models.ForeignKey(Category,on_delete=models.SET_NULL,null=True)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.SET_NULL,null=True)
+
+    title = models.CharField(max_length=100,default="Fresh Pear")
+    image = models.ImageField(upload_to=user_directory_path,default="product.jpg")
+    description = models.TextField(null=True,blank=True,default="this is a product")
+
+    price = models.DecimalField(max_digits=10,decimal_places=2,default="1.99")
+    old_price = models.DecimalField(max_digits=10,decimal_places=2,default="2.99")
+
+    specification = models.TextField(null=True,blank=True)
+    tags = TaggableManager(null = True)
+    product_status = models.CharField(choices=STATUS,max_length=20,default="in_stock")
+
+    status = models.BooleanField(default=True)
+    in_stock = models.BooleanField(default=True)
+    featured = models.BooleanField(default=False)
+    digital = models.BooleanField(default=False)
+
+    sku = ShortUUIDField(unique=True,max_lenght = 20)
+
+    date = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+
+        verbose_name_plural = 'Products'
+
+    def product_image(self):
+        return mark_safe('<img src="%s" width="50" height="50" />'%(self.image.url))
+    
+
+    def get_precentage(self):
+
+        new_price = (self.price / self.old_price)*100
+        return new_price
+    
+    def __str__(self):
+
+        return self.title
+    
+
+
+class ProductImages(models.Model):
+
+    images = models.ImageField(upload_to="product-images",default="product.jpg")
+    product = models.ForeignKey(Product,on_delete=models.SET_NULL,null=True)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+
+        verbose_name_plural="Product Images"
+
+    
+
+
 
 
