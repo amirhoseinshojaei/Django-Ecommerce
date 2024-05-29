36847fabf32ff1435b07d84848056bf29f3bf48c
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-29 23:54:53 +0330

    remove filter by featured

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,7 @@ from .models import (Product,Category,Vendor,CartOrder,CartOrderItem,Wishlist,Pr
 def index(request):
 
     # products = Product.objects.all().order_by('-id')
-    products = Product.objects.filter(product_status="published",featured=True).order_by('-id')
+    products = Product.objects.filter(product_status="published").order_by('-id')
     context = {
 
         "products":products
