9269057f8d47be6394433789aa204505a609d02e
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-05-29 22:16:51 +0330

    build index view

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from .models import (Product,Category,Vendor,CartOrder,CartOrderItem,Wishlist,ProductImages,ProductReview,Address )
 # Create your views here.
+
+
+def index(request):
+
+    # products = Product.objects.all().order_by('-id')
+    products = Product.objects.filter(product_status="published",featured=True)
+    context = {
+
+        "products":products
+    }
+
+    return render(request,"shop/index.html",context)
