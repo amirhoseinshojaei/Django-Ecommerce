9b66b2d1aca591868a66f959507336ab56fc2c93
DESKTOP-6IQPGSP\amirh <amirhoseinshojaeibu80@gmail.com>
2024-06-12 01:32:20 +0330

    add count feature to category list view

--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import (Product,Category,Vendor,CartOrder,CartOrderItem,Wishlist,ProductImages,ProductReview,Address )
+from django.db.models import Count
 # Create your views here.
 
 
@@ -28,7 +29,8 @@ def product_list_view(request):
 
 def category_list_view(request):
 
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
+    categories = Category.objects.all().annotate(product_count=Count("product"))
     context = {
 
         'categories': categories
